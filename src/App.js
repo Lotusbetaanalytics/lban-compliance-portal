02fa72fce607ffb59c27150f2bf2f94170f1d72f
Ousjiali <fonsusali@yahoo.com>
2022-07-26 08:39:40 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./Screen/LandingPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={LandingPage} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
